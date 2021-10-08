486591b316e83ad127f843ecad04a3e45b1aef27
Onandi Skeen <onandiskeen@gmail.com>
2021-10-08 00:33:41 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,21 @@
 window.onload = function(){
     let boxes = document.getElementById('board').getElementsByTagName('div');
     let xTurn = true;
-    let myArray = [];
+    let xArray = [];
+    let yArray = [];
+    let winConditions = [
+        ['0', '1', '2'],
+        ['3', '4', '5'],
+        ['6', '7', '8'],
+        ['0', '3', '6'],
+        ['1', '4', '7'],
+        ['2', '5', '8'],
+        ['0', '4', '8'],
+        ['2', '4', '6']
+    ];
 
     for(i=0; i<boxes.length; i++){
+        boxes[i].id = ''+i;
         boxes[i].classList.add('square');
         boxes[i].addEventListener('click', handleClick, { once: true});
         boxes[i].addEventListener('mouseover', hoverFunction);
@@ -15,10 +27,20 @@ window.onload = function(){
             e.target.innerHTML = 'X';
             e.target.classList.add('X');
             xTurn = false;
+            xArray.push(e.target.id);
+            if(winner(xArray)){
+                document.getElementById('status').innerHTML = 'Congratulations! X is the Winner!';
+                document.getElementById('status').classList.add('you-won');
+            }
         }else {
             e.target.innerHTML = 'O';
             e.target.classList.add('O');
             xTurn = true;
+            yArray.push(e.target.id);
+            if(winner(yArray)){
+                document.getElementById('status').innerHTML = 'Congratulations! O is the Winner!';
+                document.getElementById('status').classList.add('you-won');
+            }
         }     
     }
 
@@ -29,7 +51,20 @@ window.onload = function(){
     function leaveFunction(e){
         e.target.classList.remove('hover');
     }
-    
+
+    function winner(array){
+        for(i=0;i<winConditions.length;i++){
+            var yes = 0;
+            for(x=0;x<winConditions[i].length;x++){
+                if(array.includes(winConditions[i][x])){
+                    yes +=1;
+                    if(yes == 3){
+                        return true;
+                    }
+                } 
+            }
+        }
+    }
 };
 
 
